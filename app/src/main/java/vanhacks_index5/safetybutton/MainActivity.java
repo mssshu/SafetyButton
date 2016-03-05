eadf8469e3d0c418d8a415cde39415467817c6db
Luda Shu <ludashu.88@hotmail.com>
2016-03-05 11:51:57 -0800

    added info page

--- app/src/main/java/vanhacks_index5/safetybutton/MainActivity.java
+++ app/src/main/java/vanhacks_index5/safetybutton/MainActivity.java
@@ -1,5 +1,6 @@
 package vanhacks_index5.safetybutton;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,8 +9,10 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button b;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,14 +21,8 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-       FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        b = (Button) findViewById(R.id.button);
+
     }
 
     @Override
@@ -44,6 +41,10 @@ public class MainActivity extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            final Intent i = new Intent(this,InfoPage.class);
+
+            startActivity(i);
+
             return true;
         }
 
