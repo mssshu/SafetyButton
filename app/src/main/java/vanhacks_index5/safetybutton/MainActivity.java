a419a3580e0fb9a480d823b66c37a09a760e8d3f
Jacky Lui <jackycodes@gmail.com>
2016-03-05 11:29:35 -0800

    added locations services

--- app/src/main/java/vanhacks_index5/safetybutton/MainActivity.java
+++ app/src/main/java/vanhacks_index5/safetybutton/MainActivity.java
@@ -1,5 +1,6 @@
 package vanhacks_index5.safetybutton;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -26,6 +27,9 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        Intent intent = new Intent(MainActivity.this, LocationActivity.class);
+        startActivity(intent);
     }
 
     @Override
